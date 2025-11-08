9ed32c3b494ee573f0c892cbb19a1dbdd930e31d
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-11-08 17:49:17 +0200

    installation of firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbXf-ltp0DSD1QZagkqf0_UCkONi3_jSM",
+  authDomain: "to-do-6eaad.firebaseapp.com",
+  projectId: "to-do-6eaad",
+  storageBucket: "to-do-6eaad.firebasestorage.app",
+  messagingSenderId: "506335425345",
+  appId: "1:506335425345:web:09670a4f5d66027e25e7c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
